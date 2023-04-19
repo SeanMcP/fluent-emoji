f4fb8e6b48b8f7882638e88c38549a4c404a2cdc
Sean McPherson <git@seanmcp.com>
2023-04-19 15:37:22 -0400

    move to background image

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # `fluent-emoji`
 
 A web component that swaps standard emojis with Microsoft's Fluent Emojis
+
+There is a small layout shift when the emoji is swapped that I'm trying to remove. If you have any ideas, please open a PR or issue.
--- fluent-emoji.js
+++ fluent-emoji.js
@@ -4,14 +4,37 @@ export default class FluentEmoji extends HTMLElement {
   constructor() {
     super();
 
+    this.render();
+  }
+
+  render() {
     const emoji = this.textContent;
     const dirName = emojiMap[emoji];
 
     if (dirName) {
-      this.innerHTML = `<img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/${dirName}/3D/${this.dirNameToSlug(
-        dirName
-      )}_3d.png" aria-hidden="true" style="height: 1em; width: 1em" />`;
+      if (!this.shadowRoot) this.attachShadow({ mode: "open" });
+
+      this.setAttribute("aria-hidden", "true");
+      this.setAttribute("role", "img");
+
+      this.shadowRoot.innerHTML = `
+        <style>
+          :host {
+            background-image: url("${this.getSrc(dirName)}");
+            background-size: contain;
+            display: inline-block;
+            height: 1em;
+            transform: translateY(0.2em);
+            width: 1em;
+          }
+        </style>`;
     }
+  }
+
+  getSrc(dirName, type = "3D") {
+    return `https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/${dirName}/${type}/${this.dirNameToSlug(
+      dirName
+    )}_${type.toLowerCase()}.png`;
   }
 
   dirNameToSlug(dirName) {
