a36e1f3c8afaa083246259a859046e8c5ce373b7
Sean McPherson <git@seanmcp.com>
2023-04-25 07:13:33 -0700

    add platforms attribute
    Not convinced on this API or implementation, but it's a start.

--- fluent-emoji.js
+++ fluent-emoji.js
@@ -1,20 +1,37 @@
 import emojiMap from "./emoji-map.js";
 
 export default class FluentEmoji extends HTMLElement {
+  #height = null;
+  #width = null;
+
   constructor() {
     super();
 
     this.render();
   }
 
+  static get observedAttributes() {
+    return ["aria-label", "platforms"];
+  }
+
+  attributeChangedCallback(_, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.render();
+    }
+  }
+
   render() {
     const emoji = this.textContent;
     const dirName = emojiMap[emoji];
 
-    const textNode = this.childNodes[0];
-    const { height, width } = this.getTextNodeDimensions(textNode);
-
     if (dirName) {
+      if (!this.shouldReplaceEmoji) {
+        return;
+      }
+
+      const textNode = this.childNodes[0];
+      const { height, width } = this.getTextNodeDimensions(textNode);
+
       if (!this.shadowRoot) this.attachShadow({ mode: "open" });
 
       if (this.getAttribute("aria-label")) {
@@ -41,24 +58,41 @@ export default class FluentEmoji extends HTMLElement {
   }
 
   getSrc(dirName, type = "3D") {
-    return `https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/${dirName}/${type}/${this.dirNameToSlug(
+    const slug = dirName.replace(/ /g, "_").toLowerCase();
+    return `https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/${encodeURIComponent(
       dirName
-    )}_${type.toLowerCase()}.png`;
+    )}/${type}/${slug}_${type.toLowerCase()}.png`;
   }
 
   // Modified from https://stackoverflow.com/a/6966613
   getTextNodeDimensions(textNode) {
-    const range = document.createRange();
-    range.selectNodeContents(textNode);
-    const rect = range.getBoundingClientRect();
+    if (!this.#height) {
+      const range = document.createRange();
+      range.selectNodeContents(textNode);
+      const rect = range.getBoundingClientRect();
+
+      this.#height = rect.height;
+      this.#width = rect.width;
+    }
 
     return {
-      height: rect.height,
-      width: rect.width,
+      height: this.#height,
+      width: this.#width,
     };
   }
 
-  dirNameToSlug(dirName) {
-    return dirName.replace(/ /g, "_").toLowerCase();
+  get shouldReplaceEmoji() {
+    const platforms = this.getAttribute("platforms");
+    if (!platforms) return true;
+
+    if (
+      (platforms.includes("windows") && navigator.platform.includes("Win")) ||
+      (platforms.includes("mac") && navigator.platform.includes("Mac")) ||
+      (platforms.includes("linux") && navigator.platform.includes("Linux"))
+    ) {
+      return true;
+    }
+
+    return false;
   }
 }
